c1d5441a1c48dd2eec16689d441c663c502024ea
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-21 19:17:26 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -31,10 +31,10 @@ class TicTacToe
   end
   
   def position_taken?(board, index)
-  if (board[index] == " " || board[index] == "" || board[index] == nil)
-    false
-  else
-    true
-  end
+    if (board[index] == " " || board[index] == "" || board[index] == nil)
+      false
+    else
+      true
+    end
   end
 end
