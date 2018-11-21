f68a0aefa5dbbe257b05b8a206d151db1dd1862f
Kimberly Asgill <kimberlyasgill@gmail.com>
2018-11-21 19:19:54 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -38,8 +38,8 @@ class TicTacToe
     end
   end
   
-  def valid_move?(board, index)
-    if (position_taken?(board, index) == false && index.between?(0, 8))
+  def valid_move?(index)
+    if (position_taken?(index) == false && index.between?(0, 8))
       true
     else
       false
